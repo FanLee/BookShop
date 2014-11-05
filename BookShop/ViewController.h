ea78ae47af2df3e49e387f05772fa7d1e0b8ec1d
FanLee <fanlee88@gmail.com>
2014-11-05 22:38:32 +0300

    Initial commit

---
+++ BookShop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BookShop
+//
+//  Created by FanLee on 05.11.14.
+//  Copyright (c) 2014 Fan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BookShop/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BookShop
+//
+//  Created by FanLee on 05.11.14.
+//  Copyright (c) 2014 Fan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
