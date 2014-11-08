95d0afbcbf57a0f354ad74d03f8969732a286430
FanLee <fanlee88@gmail.com>
2014-11-08 19:32:17 +0300

    implementation of textFielddidbeginediting

--- BookShop/ViewController.m
+++ BookShop/ViewController.m
@@ -10,18 +10,43 @@
 
 @interface ViewController ()
 
+
 @end
 
 @implementation ViewController
 
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.emailTextField.delegate=self;
     // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)textFieldDidBeginEditing:(UITextField *)emailTextField{
+    [self animatedTextField: emailTextField up:YES];
+}
+
+- (IBAction)em:(id)sender {
+}
+
+-(void)animatedTextField:(UITextField*)animatedTextField up:(BOOL) up
+{
+    const int movementDistance = 100;
+    const float movementDuration = 0.3f;
+    
+    int movement = (up ? -movementDistance : movementDistance);
+    
+    [UIView beginAnimations:@"anim" context:nil];
+    [UIView setAnimationBeginsFromCurrentState:YES];
+    [UIView setAnimationDuration:movementDuration];
+    self.view.frame=CGRectOffset(self.view.frame, 0, movement);
+    [UIView commitAnimations];
 }
 
 @end
