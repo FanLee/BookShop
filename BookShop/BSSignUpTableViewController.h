aeb8932adeb41bf2045a07618c93805ebd115470
FanLee <fanlee88@gmail.com>
2014-11-14 00:13:53 +0300

    dveloping signUpViewController

---
+++ BookShop/BSSignUpTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignUpTableViewController.h
+//  BookShop
+//
+//  Created by FanLee on 13.11.14.
+//  Copyright (c) 2014 Fan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BSSignUpTableViewController : UITableViewController
+//@property(nonatomic,strong) *downDropMenu;
+@end
