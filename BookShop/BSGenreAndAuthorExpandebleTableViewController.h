12d1dacae90c28b9889058e12a3bc04170bc674f
FanLee <fanlee88@gmail.com>
2015-01-15 21:27:51 +0300

    add accordion menu

---
+++ BookShop/BSGenreAndAuthorExpandebleTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  BSGenreAndAuthorExpandebleTableViewController.h
+//  BookShop
+//
+//  Created by FanLee on 13.01.15.
+//  Copyright (c) 2015 Fan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BSGenreAndAuthorExpandebleTableViewController : UITableViewController
+
+@end
